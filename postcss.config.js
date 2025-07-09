ac292a88dfe748e9485488b7e4701487a463de1b
A.HARBI <87930239+hasnape@users.noreply.github.com>
2025-07-10 00:35:13 +0200

    Initial commit depuis Nexus-Help-Desk-N1-master

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: c:\Users\HARBI Amine\Documents\Nexus-Help-Desk-N1-master\postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// filepath: c:\Users\HARBI Amine\Documents\Nexus-Help-Desk-N1-master\tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
