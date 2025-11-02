71408218287960cf0b8f7398c5014bff52708f24
Amine.H <87930239+hasnape@users.noreply.github.com>
2025-11-02 18:52:34 +0100

    Refine quota displays using RPC data

--- pages/DashboardPage.tsx
+++ pages/DashboardPage.tsx
@@ -5,7 +5,7 @@ import TicketCard from '../components/TicketCard';
 import { Button } from '../components/FormElements';
 import { useLanguage } from '../contexts/LanguageContext';
 import FloatingActionButton from '../components/FloatingActionButton';
-import PlanLimits from '../components/planLimits';
+import PlanLimits from '../components/PlanLimits';
 
 const PlusIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" {...props}>
