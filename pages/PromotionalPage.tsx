40af68a23c9b7bedee60274a54dc2a70d0d1fe90
Amine.H <87930239+hasnape@users.noreply.github.com>
2025-11-03 15:19:49 +0100

    Update PromotionalPage.tsx

--- pages/PromotionalPage.tsx
+++ pages/PromotionalPage.tsx
@@ -5,9 +5,6 @@ import { Link, useLocation } from 'react-router-dom';
 import { useApp } from '../App';
 
 
-
-import { useTranslation } from "react-i18next";
-
 const ArrowLeftIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" {...props}>
     <path fillRule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clipRule="evenodd" />
