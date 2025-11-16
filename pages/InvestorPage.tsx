982e1e8bab0aba7e2d93cb440a677a22ae3cc45b
Amine.H <87930239+hasnape@users.noreply.github.com>
2025-11-16 21:31:17 +0100

    Update InvestorPage.tsx

--- pages/InvestorPage.tsx
+++ pages/InvestorPage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Layout from "../components/Layout";
 
 const InvestorPage: React.FC = () => {
   const { t } = useTranslation();
